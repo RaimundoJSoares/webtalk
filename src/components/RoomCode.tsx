1a44f3bf41d87403103e04225f147acaa91fdeee
RaimundoJSoares <raianimes1000@gmail.com>
2022-03-13 19:49:26 -0400

    testando enviar perguntas

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,7 +1,21 @@
-import { Button } from "./Button";
+import copyIMG from '../assets/images/copy.svg'
+import '../styles/roomCode.scss'
 
-export function RoomCode(){
+type RoomcodeProps = {
+  code : string;
+}
+
+export function RoomCode( props: RoomcodeProps){
+  function copyRoomCodeToClipBoard() {
+    navigator.clipboard.writeText(props.code)
+  }
+
   return(
-    <Button></Button>
+    <button className='room-code' onClick={copyRoomCodeToClipBoard}>
+      <div>
+        <img src={copyIMG} alt="copy room code" />
+      </div>
+      <span>sala #{props.code}</span>
+    </button>
   )
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,51 @@
 import Logo from '../assets/images/WebTalk3.png'
+import { useParams} from 'react-router-dom'
 import { Button } from '../components/Button'
+import { RoomCode } from '../components/RoomCode'
 import '../styles/room.scss'
+import { FormEvent, useState } from 'react'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+
+export type Myparams ={
+  id: string;
+}
 
 export function Room(){
+  const {user} = useAuth()
+  const params = useParams<Myparams>() as Myparams;
+  const roomID = params.id
+  const [newQuestion, setNewQuestion] = useState('')
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === '') {
+      return;
+    }
+    if (!user) {
+      throw new Error('Login Necessário')
+    }
+    
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighligthed: false,
+      isAnswered: false,
+    }
+
+    await database.ref(`rooms/${roomID}/questions`).push(question);
+  }
+  
   return(
     <div id="page-room">
       <header>
         <div className="content">
           <img src={Logo} alt = 'logo'/>
-          <div>codigo</div>
+          <RoomCode code={roomID}/>
         </div>
       </header>
 
@@ -17,13 +54,15 @@ export function Room(){
           <h1>Sala React</h1>
           <span>4 perguntas</span>
         </div>
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea
             placeholder='O que você quer perguntar?'
+            onChange={event => setNewQuestion(event.target.value)}
+            value = {newQuestion}
           />
           <div className="form-footer">
             <span>Para enviar uma pergunta, <button>faças seu login</button>.</span>
-            <Button type='submit'>Enviar pergunta</Button>
+            <Button type='submit' disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
 
